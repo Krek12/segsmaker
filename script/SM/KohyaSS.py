dde614d2aae1a8a65758d38fe0c440b77026703e
Krek12 <134212972+Krek12@users.noreply.github.com>
2024-11-11 21:16:57 +0500

    Update KohyaSS.py

--- script/SM/KohyaSS.py
+++ script/SM/KohyaSS.py
@@ -41,9 +41,9 @@ def webui_req():
         subprocess.run(shlex.split(lines), stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
     scripts = [
-        f"https://github.com/gutris1/segsmaker/raw/main/script/SM/venv.py {WEBUI}",
-        f"https://github.com/gutris1/segsmaker/raw/main/script/SM/Launcher.py {WEBUI}",
-        f"https://github.com/gutris1/segsmaker/raw/main/script/SM/segsmaker.py {WEBUI}"]
+        f"https://github.com/Krek12/segsmaker/raw/main/script/SM/venv.py {WEBUI}",
+        f"https://github.com/Krek12/segsmaker/raw/main/script/SM/Launcher.py {WEBUI}",
+        f"https://github.com/Krek12/segsmaker/raw/main/script/SM/segsmaker.py {WEBUI}"]
 
     for items in scripts:
         download(items)
